c58cdbe5d5e4b233dc066641573dcb5e71d03106
andrewfiorillo <fiorilloaj@gmail.com>
2016-06-07 15:37:06 -0400

    added support for transparent colors

--- Sketch Palettes.sketchplugin/Contents/Sketch/sketchPalettes.js
+++ Sketch Palettes.sketchplugin/Contents/Sketch/sketchPalettes.js
@@ -1,52 +1,69 @@
 
+//-------------------------------------------------------------------------------------------------------------
 // Load color palette
+//-------------------------------------------------------------------------------------------------------------
 
 function loadColors(context, target) {
 	
 	var app = NSApp.delegate();
 	var doc = context.document;
-	var inspector = doc.inspectorController();
-	var openPanel = NSOpenPanel.openPanel();
+	var open = NSOpenPanel.openPanel();
 	var version = context.plugin.version().UTF8String();
-	var fileTypes = [NSArray arrayWithObjects:@"sketchpalette",nil]
+	var fileTypes = [NSArray arrayWithObjects:@"sketchpalette",nil];
 	
 	// Open file picker to choose palette file
-	openPanel.setCanChooseDirectories(true);
-	openPanel.setAllowedFileTypes(fileTypes);
-	openPanel.setCanChooseFiles(true);
-	openPanel.setCanCreateDirectories(true);
-	openPanel.setTitle("Choose a file");
-	openPanel.setPrompt("Choose");
-	openPanel.runModal();
 	
-	// Get file path to file selected
-	var filePath = openPanel.URLs().firstObject().path();
+	open.setAllowedFileTypes(fileTypes);
+	open.setCanChooseDirectories(true);
+	open.setCanChooseFiles(true);
+	open.setCanCreateDirectories(true);
+	open.setTitle("Choose a file");
+	open.setPrompt("Choose");
+	open.runModal();
 	
 	// Read contents of file into NSString, then to JSON
+	
+	var filePath = open.URLs().firstObject().path();
 	var fileContents = NSString.stringWithContentsOfFile(filePath);
 	var paletteContents = JSON.parse(fileContents.toString());
 	var palette = paletteContents.colors;
 	var compatibleVersion = paletteContents.compatibleVersion;
+	
+	// Check if plugin is out of date anf incompatible with a newer palette version
 	
 	if (compatibleVersion && compatibleVersion > version) {
 		NSApp.displayDialog("Your plugin out of date. Please update to the latest version of Sketch Palettes.");
 		return;
 	}
 	
-	// Convert hex strings into MSColors
-	var mspalette = [];
-	for (var i = 0; i < palette.length; i++) {
-		mspalette.push(MSColor.colorWithSVGString(palette[i]));
-	};
+	// Convert colors to MSColors
+	// Check for older hex code palette version
 	
-	// Convert array into MSArray
-	var colors = MSArray.dataArrayWithArray(mspalette);
+	var mspalette = [];
+	
+	if (!compatibleVersion || compatibleVersion < 1.4) {
+		for (var i = 0; i < palette.length; i++) {
+			mspalette.push(MSColor.colorWithSVGString(palette[i]));
+		};
+	} else {
+		for (var i = 0; i < palette.length; i++) {
+			mspalette.push(MSColor.colorWithRed_green_blue_alpha(
+				palette[i].red,
+				palette[i].green,
+				palette[i].blue,
+				palette[i].alpha
+			));	
+		};
+	}
 	
 	// Load colors in target color picker section
-	if (target == "document") {
-		doc.documentData().assets().setColors(colors);
-	} else if (target == "global" ) {
+	
+	var colors = MSArray.dataArrayWithArray(mspalette);
+	
+	if (target == "global") {
 		app.globalAssets().setColors(colors);
+	} else if (target == "document" ) {
+		doc.documentData().assets().setColors(colors);
 	}
 	
 	app.refreshCurrentDocument();
@@ -55,9 +72,9 @@ function loadColors(context, target) {
 
 
 //-------------------------------------------------------------------------------------------------------------
-
-
 // Save color palette
+//-------------------------------------------------------------------------------------------------------------
+
 
 function saveColors(context,target) {
 	
@@ -66,50 +83,58 @@ function saveColors(context,target) {
 	var version = context.plugin.version().UTF8String();
 	
 	// Get colors from target color picker section
-	if (target == "document") {
-		var colors = doc.documentData().assets().colors();
-	} else if (target == "global"){
+	
+	if (target == "global") {
 		var colors = app.globalAssets().colors()	
+	} else if (target == "document") {
+		var colors = doc.documentData().assets().colors();
 	}
 	
 	// Only run if there are colors
-	if (colors.count() > 0) {
+	
+	if (colors.count() > 0) {	
 		
-		// Save panel settings
-		var savePanel = NSSavePanel.savePanel();
-		savePanel.setNameFieldStringValue("untitled.sketchpalette");
-		savePanel.setAllowedFileTypes([@"sketchpalette"]);
-		savePanel.setAllowsOtherFileTypes(false);
-		savePanel.setExtensionHidden(false);
+		var save = NSSavePanel.savePanel();
+		save.setNameFieldStringValue("untitled.sketchpalette");
+		save.setAllowedFileTypes([@"sketchpalette"]);
+		save.setAllowsOtherFileTypes(false);
+		save.setExtensionHidden(false);
 		
 		// Open save dialog and run if Save was clicked
-		if (savePanel.runModal()) {
+		
+		if (save.runModal()) {
 			
-			// Convert MSArray into array
+			// Convert MSColors to rgba
+			
 			var mspalette = colors.array();
-			
-			// Convert MSColors into hex strings
 			var palette = [];
+			
 			for (var i = 0; i < mspalette.count(); i++) {
-				palette.push("#" + mspalette[i].hexValue());
+				palette.push({
+					red: mspalette[i].red(),
+					green: mspalette[i].green(),
+					blue: mspalette[i].blue(),
+					alpha: mspalette[i].alpha()	
+				});
 			};
 			
 			// Palette data
-			var fileJSON = {
-				"compatibleVersion": "1.0", // minimum plugin version required to load palette
-				"pluginVersion": version, // version of plugin used when saving
+
+			var fileData = {
+				"compatibleVersion": "1.4", // min plugin version to load palette
+				"pluginVersion": version, //  plugin version used to save palette
 				"colors": palette
 			}
 			
-			// Convert palette data to string
-			var fileContents = JSON.stringify(fileJSON);
-			var fileString = NSString.stringWithString(JSON.stringify(fileJSON));
-			
 			// Get chosen file path
-			var filePath = savePanel.URL().path();
+			
+			var filePath = save.URL().path();
 			
 			// Write file to specified file path
-			[fileString writeToFile:filePath atomically:true encoding:NSUTF8StringEncoding error:null];
+			
+			var file = NSString.stringWithString(JSON.stringify(fileData));
+			
+			[file writeToFile:filePath atomically:true encoding:NSUTF8StringEncoding error:null];
 
 		}
 		
@@ -119,24 +144,7 @@ function saveColors(context,target) {
 
 
 //-------------------------------------------------------------------------------------------------------------
-
-
-// Document Colors 
-
-function loadDocumentPalette(context) {
-	loadColors(context, "document");
-}
-
-function saveDocumentPalette(context) {
-	saveColors(context, "document");
-}
-
-function clearDocumentPalette(context) {	
-	var doc = context.document;
-	doc.documentData().assets().setColors(MSArray.dataArrayWithArray([]));
-}
-
-
+// Menu Items
 //-------------------------------------------------------------------------------------------------------------
 
 
@@ -153,5 +161,21 @@ function saveGlobalPalette(context) {
 function clearGlobalPalette(context) {	
 	var app = NSApp.delegate();
 	app.globalAssets().setColors(MSArray.dataArrayWithArray([]));
+}
+
+
+// Document Colors 
+
+function loadDocumentPalette(context) {
+	loadColors(context, "document");
+}
+
+function saveDocumentPalette(context) {
+	saveColors(context, "document");
+}
+
+function clearDocumentPalette(context) {	
+	var doc = context.document;
+	doc.documentData().assets().setColors(MSArray.dataArrayWithArray([]));
 }
 
